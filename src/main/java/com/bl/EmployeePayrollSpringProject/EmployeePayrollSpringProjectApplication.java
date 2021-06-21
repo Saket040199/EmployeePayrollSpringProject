be80042f2337b0e17381f7c328a26ad47513459e
saket <saketj420@gmail.com>
2021-06-21 13:32:34 +0530

    Setting all dependencies used in spring project

---
+++ src/main/java/com/bl/EmployeePayrollSpringProject/EmployeePayrollSpringProjectApplication.java
@@ -0,0 +1,14 @@
+package com.bl.EmployeePayrollSpringProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayrollSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePayrollSpringProjectApplication.class, args);
+		System.out.println("Employee Payroll Deployed Successfully");
+	}
+
+}
---
+++ src/test/java/com/bl/EmployeePayrollSpringProject/EmployeePayrollSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bl.EmployeePayrollSpringProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeePayrollSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
