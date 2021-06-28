37c780993e53b92cf23d36509d08a234eb440bb7
saket <saketj420@gmail.com>
2021-06-28 13:48:15 +0530

    Validating user name and salary

--- src/main/java/com/bl/EmployeePayrollSpringProject/controller/EmployeePayrollController.java
+++ src/main/java/com/bl/EmployeePayrollSpringProject/controller/EmployeePayrollController.java
@@ -2,6 +2,8 @@ package com.bl.EmployeePayrollSpringProject.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -45,7 +47,7 @@ public class EmployeePayrollController {
 
 
 	@PostMapping("/create")
-	public ResponseEntity<ResponseDTO> addEmployeePayrollData(@RequestBody EmployeePayrollDTO employeePayrollDTO){
+	public ResponseEntity<ResponseDTO> addEmployeePayrollData(@Valid @RequestBody EmployeePayrollDTO employeePayrollDTO){
 		EmployeePayrollData employeePayrollData = employeePayrollService.createEmployeePayrollData(employeePayrollDTO);
 		ResponseDTO respDTO = new ResponseDTO("Create EmployeePayroll Data Successfull", employeePayrollData);
 		return new ResponseEntity<ResponseDTO>(respDTO, HttpStatus.OK);
@@ -53,7 +55,7 @@ public class EmployeePayrollController {
 
 
 	@PutMapping("/update/{empId}")
-	public ResponseEntity<ResponseDTO> updateEmployeePayrollData(@PathVariable("empId") int empId, @RequestBody EmployeePayrollDTO employeePayrollDTO){
+	public ResponseEntity<ResponseDTO> updateEmployeePayrollData(@Valid @PathVariable("empId") int empId, @RequestBody EmployeePayrollDTO employeePayrollDTO){
 		EmployeePayrollData employeePayrollData = employeePayrollService.updateEmployeePayrollData(empId, employeePayrollDTO);
 		ResponseDTO respDTO = new ResponseDTO("Create AddressBOOK Data Successfull", employeePayrollData);
 		return new ResponseEntity<ResponseDTO>(respDTO, HttpStatus.OK);
--- src/main/java/com/bl/EmployeePayrollSpringProject/employeepayrollDTO/EmployeePayrollDTO.java
+++ src/main/java/com/bl/EmployeePayrollSpringProject/employeepayrollDTO/EmployeePayrollDTO.java
@@ -1,8 +1,18 @@
 package com.bl.EmployeePayrollSpringProject.employeepayrollDTO;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Pattern;
+
+import lombok.Data;
+
+@Data
 public class EmployeePayrollDTO {
 
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message= " InValid Name")
 	public String name;
+    @Min(value=500, message="Salary is less than 500")
+    @Max(value=1000000, message="Salary cannot be greater than 1000000")
 	public long salary;
 	public EmployeePayrollDTO(String name, long i) {
 		super();
