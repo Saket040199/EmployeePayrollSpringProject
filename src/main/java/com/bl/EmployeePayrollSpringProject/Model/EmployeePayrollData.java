c630abece1d82ccc9c33671cdcebc63aa027b3a7
saket <saketj420@gmail.com>
2021-06-29 11:54:47 +0530

    Adding remaining properties

--- src/main/java/com/bl/EmployeePayrollSpringProject/Model/EmployeePayrollData.java
+++ src/main/java/com/bl/EmployeePayrollSpringProject/Model/EmployeePayrollData.java
@@ -1,5 +1,8 @@
 package com.bl.EmployeePayrollSpringProject.Model;
 
+
+import java.util.List;
+
 import javax.persistence.Id;
 
 import com.bl.EmployeePayrollSpringProject.employeepayrollDTO.EmployeePayrollDTO;
@@ -12,7 +15,13 @@ public class EmployeePayrollData {
 	@Id
 	private int employeePayrollId;
 	private String name;
-	private Long salary;
+	private long salary;
+	private String gender;
+	private String startDate;
+	private String note;
+	private String profile;
+	private List<String> department;
+
 	
 	public EmployeePayrollData() {
 		// TODO Auto-generated constructor stub
@@ -23,5 +32,10 @@ public class EmployeePayrollData {
 		this.employeePayrollId = employeePayrollId;
 		this.name = employeePayrollDTO.name;
 		this.salary = employeePayrollDTO.salary;
+		this.gender = employeePayrollDTO.gender;
+		this.note = employeePayrollDTO.note;
+		this.startDate = employeePayrollDTO.startDate;
+		this.profile = employeePayrollDTO.profile;
+		this.department = employeePayrollDTO.department;
 	}
 }
--- src/main/java/com/bl/EmployeePayrollSpringProject/employeepayrollDTO/EmployeePayrollDTO.java
+++ src/main/java/com/bl/EmployeePayrollSpringProject/employeepayrollDTO/EmployeePayrollDTO.java
@@ -1,5 +1,8 @@
 package com.bl.EmployeePayrollSpringProject.employeepayrollDTO;
 
+
+import java.util.List;
+
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.Pattern;
@@ -14,13 +17,32 @@ public class EmployeePayrollDTO {
     @Min(value=500, message="Salary is less than 500")
     @Max(value=1000000, message="Salary cannot be greater than 1000000")
 	public long salary;
-	public EmployeePayrollDTO(String name, long i) {
-		super();
-		this.name = name;
-		this.salary = i;
-	}
+    
+    public String gender;
+    public String startDate;
+    public String note;
+    public String profile;
+    public List<String> department;
+    
+    
+	
 	public EmployeePayrollDTO() {
 		// TODO Auto-generated constructor stub
+	}
+
+
+
+	public EmployeePayrollDTO( String name,
+			 long salary,
+			String gender, String startDate, String note, String profile, List<String> department) {
+		super();
+		this.name = name;
+		this.salary = salary;
+		this.gender = gender;
+		this.startDate = startDate;
+		this.note = note;
+		this.profile = profile;
+		this.department = department;
 	}
 	
 	
